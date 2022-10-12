c648cee82250c5854ccac03835add743a29aee94
Züleyha Kurt <netice2017@gmail.com>
2022-10-12 20:38:09 +0300

    edit

--- src/components/1-events/Event.jsx
+++ src/components/1-events/Event.jsx
@@ -1,10 +1,24 @@
 
 
 const Event = () => {
+
+    const handleClick = () => {
+    alert("Btn Clicked")
+    }
+    const handleClear=(msg) => {
+        alert(msg)
+    }
+    const handleChange = (e) => { 
+    console.log(e.target)
+    }
+
   return (
       <div className="container text-center mt-4">
-          <button className="btn btn-success">Click</button>
-    </div>
+          <button onClick={handleClick}  className="btn btn-success">Click</button>
+    
+          <button onClick={()=> handleClear("Clear btn clicked")} className="btn btn-dark">Clear</button>
+      <button onClick={handleChange} className="btn btn-danger">Change</button>
+      </div>
   )
 }
 
