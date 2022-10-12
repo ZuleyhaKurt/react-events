db65293b98ca73f02a88c9b4e6667480497f2b83
Züleyha Kurt <netice2017@gmail.com>
2022-10-12 23:02:16 +0300

    finish

--- src/components/usestate/UsestateCounter.jsx
+++ src/components/usestate/UsestateCounter.jsx
@@ -2,18 +2,32 @@ import { useState } from "react";
 
 const UsestateCounter = () => {
 const[count,setCount]=useState(0)
+    
+    const [person, setPerson] = useState({
+        name: "John",
+        surname: "Doe",
+        age:43,
+})
+    
+    
+    
+    const incAge = () => {
+       setPerson({...person, age:person.age+1})
+   } 
+    
     const increment = () => { 
         setCount(count+1)
     }
-    const dec = () => {
-        if (count > 0) {
-            setCount(count-1)
-        }
-    }
+    // const dec = () => {
+    //     if (count > 0) {
+    //         setCount(count-1)
+    //     }
+    // }
 
   return (
       
-          <div className="container text-center mt-4">
+      <div className="container text-center mt-4">
+          <section>
         <h1>Usestate Hook</h1>
           <h2 className="display-4 text-danger">COUNT:{ count}</h2>
         <button onClick={increment}  className="btn btn-success">INC</button>
@@ -21,9 +35,19 @@ const[count,setCount]=useState(0)
        
 {/*        
           <button onClick={dec} className="btn btn-danger">DEC</button> */}
-          <button onClick={()=> count>0 && setCount(count-1)}  className="btn btn-danger">DEC</button>
+          <button onClick={() => count > 0 && setCount(count - 1)} className="btn btn-danger">DEC</button>
+          </section>
+          <section>
+              <h1>UseState Object</h1>
+              <h2>{person.name}</h2>
+              <h2>{person.surname}</h2>
+              <h2>{person.age}</h2>
+              <button onClick={incAge} className="btn btn-info">inc age</button>
+          </section>
       </div>
+      
     
+      
   )
 }
 
