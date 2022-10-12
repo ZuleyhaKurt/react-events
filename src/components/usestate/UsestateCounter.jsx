b646adab0c0a05497531c1163349503fb8f6fcc8
Züleyha Kurt <netice2017@gmail.com>
2022-10-12 22:43:17 +0300

    edit

--- src/components/usestate/UsestateCounter.jsx
+++ src/components/usestate/UsestateCounter.jsx
@@ -1,13 +1,27 @@
 import { useState } from "react";
 
 const UsestateCounter = () => {
+const[count,setCount]=useState(0)
+    const increment = () => { 
+        setCount(count+1)
+    }
+    const dec = () => {
+        if (count > 0) {
+            setCount(count-1)
+        }
+    }
+
   return (
       
           <div className="container text-center mt-4">
         <h1>Usestate Hook</h1>
-                <h2 className="display-4 text-danger">COUNT:</h2>
-        <button  className="btn btn-success">INC</button>
-        <button className="btn btn-danger">DEC</button>
+          <h2 className="display-4 text-danger">COUNT:{ count}</h2>
+        <button onClick={increment}  className="btn btn-success">INC</button>
+        <button onClick={()=>setCount(0)}  className="btn btn-dark">CLR</button>
+       
+       
+          <button onClick={dec} className="btn btn-danger">DEC</button>
+          {/* <button onClick={()=> setCount(count-1)}  className="btn btn-danger">DEC</button> */}
       </div>
     
   )
