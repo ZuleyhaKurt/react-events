4083caaefd8da3b5c654aea0fe6a3024d7a69da1
Züleyha Kurt <netice2017@gmail.com>
2022-10-12 22:23:25 +0300

    edit

--- src/components/usestate/UsestateCounter.jsx
+++ src/components/usestate/UsestateCounter.jsx
@@ -1,13 +1,12 @@
-
+import { useState } from "react";
 
 const UsestateCounter = () => {
   return (
       
           <div className="container text-center mt-4">
-        <h1>CLASSFUL COMPONENTS</h1>
+        <h1>Usestate Hook</h1>
                 <h2 className="display-4 text-danger">COUNT:</h2>
         <button  className="btn btn-success">INC</button>
-        <button className="btn btn-dark">CLR</button>
         <button className="btn btn-danger">DEC</button>
       </div>
     
